06d1b0dcc7fc59de2bfdd79ef9bf2cf4c44008d2
Andrew Harrington <eightbitandrew@gmail.com>
2018-11-05 20:32:47 -0800

    fix for mailer

--- php_mailer/email.config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('EMAIL_USER','andrewharringtonmail@gmail.com');
-define('EMAIL_PASS','fattubolard3');
-define('EMAIL_USERNAME', 'Andrew');
-define('EMAIL_TO_ADDRESS', 'eightbitandrew@gmail.com');
-?>
